39c1a8d4af8fce77df54167860f796758e535567
quantumomid <omid.wakili@outlook.com>
2022-01-25 18:11:54 +0000

    add newsletter api route for email subscription

--- components/input/NewsletterRegistration.js
+++ components/input/NewsletterRegistration.js
@@ -1,13 +1,30 @@
+import { useRef } from "react";
 import styles from "./NewsletterRegistration.module.css";
 
 const NewsletterRegistration = () => {
 
-    const registrationHandler = (event) => {
+    // use REF since we are only accessing the email input once i.e.
+    // during submission
+    const emailRef = useRef();
+
+    const registrationHandler = async (event) => {
         event.preventDefault();
     
         // fetch user input (state or refs)
+        const enteredEmail = emailRef.current.value;
         // optional: validate input
         // send valid data to API
+
+        const postResponse = await fetch("/api/newsletter", {
+            method: "POST",
+            body: JSON.stringify({email: enteredEmail}),
+            headers: {
+                "Content-Type": "application/json",
+            },
+        });
+
+        const data = await postResponse.json();
+        console.log({data});
     }
 
     return (
@@ -18,6 +35,7 @@ const NewsletterRegistration = () => {
                     <input
                         type='email'
                         id='email'
+                        ref={emailRef}
                         placeholder='Your email'
                         aria-label='Your email'
                     />
