ccfd49c4656a1b3c717e2fb575b06a2a0389ce14
quantumomid <omid.wakili@outlook.com>
2022-01-25 18:38:42 +0000

    add comment dynamic API route

---
+++ pages/api/comments/[eventID].js
@@ -0,0 +1,45 @@
+
+
+const commentsHandler = (req, res) => {
+
+    // The parameter is called eventID since I named this file as such!
+    const eventID = req.query.eventID;
+
+    if(req.method === "POST") {
+        const { email, name, text } = req.body;
+        // server side validation
+        if(
+            !email || 
+            !email.includes("@") || 
+            !name || 
+            name.trim() === "" ||
+            !text ||
+            text.trim() === ""
+            ) {
+                res.status(422).json({ message: "Invalid input" });
+                return;
+        }
+
+        const newComment = {
+            id: new Date().toISOString(),
+            email,
+            name, 
+            text,
+        }
+        console.log({ newComment });
+
+        res.status(201).json({ message: "Comment successfully added!", comment: newComment });
+    }
+
+    if(req.method === "GET") {
+
+        const dummyList = [
+            { id: "c1", name: "John", comment: "Did not like this!" },
+            { id: "c2", name: "Stacey", comment: "It was okay" }
+        ];
+
+        res.status(200).json({ comments: dummyList });
+    }
+}
+
+export default commentsHandler;
