65089be6b1f740a31b58185d2b9f685e65772f94
quantumomid <omid.wakili@outlook.com>
2022-01-26 07:35:39 +0000

    connected to mongoDB from Next.js server side api route

--- pages/api/newsletter.js
+++ pages/api/newsletter.js
@@ -1,5 +1,6 @@
+import { MongoClient } from "mongodb";
 
-const newsletterHandler = (req, res) => {
+const newsletterHandler = async (req, res) => {
     if(req.method === "POST") {
         const userEmail = req.body.email;
 
@@ -8,7 +9,20 @@ const newsletterHandler = (req, res) => {
             return;
         }
 
-        console.log({userEmail});
+        const client = await MongoClient.connect(
+            `mongodb+srv://quantumomid:${process.env.MONGO_DB_USER_PASSWORD}@cluster0.garht.mongodb.net/newsletter?retryWrites=true&w=majority`
+        );
+
+        // connect to the database
+        const db = client.db();
+
+        // get specific collection from database
+        await db.collection("emails").insertOne({ email: userEmail });
+
+        //disconnect from client once done!
+        client.close();
+
+        // console.log({userEmail});
         res.status(201).json({ message: "Signed up!" });
     }
 }
